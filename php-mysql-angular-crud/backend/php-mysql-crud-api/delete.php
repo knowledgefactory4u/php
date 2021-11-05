d0b68ce3ea05f22515897542a80e6e8ab1a66aec
knowledgefactory4u <54763130+knowledgefactory4u@users.noreply.github.com>
2021-11-06 00:32:54 +0530

    Update delete.php

--- php-mysql-angular-crud/backend/php-mysql-crud-api/delete.php
+++ php-mysql-angular-crud/backend/php-mysql-crud-api/delete.php
@@ -4,8 +4,7 @@
     header("Content-Type: application/json; charset=UTF-8");
     header("Access-Control-Allow-Methods: GET,POST,PUT,DELETE");
     header("Access-Control-Max-Age: 3600");
-    header("Access-Control-Allow-Headers: Content-Type, 
-        Access-Control-Allow-Headers, Authorization, X-Requested-With");
+    header("Access-Control-Allow-Headers: Content-Type,Access-Control-Allow-Headers, Authorization, X-Requested-With");
    
       
     include_once 'config/database.php';
